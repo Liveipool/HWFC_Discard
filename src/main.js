9652577ad07b9c09caff4c6a53a3870aef8a88c8
liveipool <liveipool@liveipool-CW35S.lan>
2016-11-23 00:18:48 +0800

    add sass

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import routes from './routes'
+import style from './style'
 
 const app = new Vue({
   el: '#app',
--- src/pages/Home/components/Left.vue
+++ src/pages/Home/components/Left.vue
@@ -1,5 +1,6 @@
 <template>
-	<div class="lallana">
+	<div class="lallana mt-100 ml-20">
+		<div class="fs-40 m-10 c-hwfc-blue">lallana</div>
 		<img src="../../../assets/08685344-11d9-426d-a204-ccabbf7049ba-2060x1236.jpeg">
 	</div>
 </template>
@@ -11,8 +12,5 @@
 </script>
 
 <style>
-	.lallana {
-		margin-top: 100px;
-		margin-left: 20px;
-	}
+
 </style>
